b255b2cd88e526a7ab3e1c5da18444e94b16fe28
Tricholome <Tricholome@users.noreply.github.com>
2026-08-12 14:07:41 +0000

    deploy: Tricholome/rootelo@867264ad887c2ec5fbbc96a0c961d15822e9bf7b

--- static/script.js
+++ static/script.js
@@ -249,20 +249,19 @@ $(document).ready(function() {
 	// --- 2. MATCHES ---
 	if ($('#matchesTable').length > 0) {
 		$('#matchesTable').DataTable({
-			"order": [[0, "desc"]], 
+			"order": [[1, "desc"]], 
 			"responsive": true,
 			"pageLength": 50,
 			"dom": 'rt<"bottom"p><"clear">',
 			"columnDefs": [
-				{ "targets": 0, "visible": false }, // Masque la colonne de texte du groupe
-				{ "className": "player-name-cell", "targets": 1 },
-				{ "className": "numeric-cell", "targets": [2, 3, 4, 5] },
-				{ "responsivePriority": 1, "targets": [1, 5] },
-				{ "responsivePriority": 2, "targets": 6 }
-			],
-			"rowGroup": {
-				"dataSrc": 0 // Active le regroupement par le Match ID / Date
-			}
+				{ "className": "rank-cell", "targets": 0 },
+				{ "className": "elo-sum-cell", "targets": 1 },
+				{ "className": "date-cell", "targets": 2 },
+				{ "className": "numeric-cell", "targets": [0, 1, 2, 4] },
+				{ "responsivePriority": 1, "targets": [1, 3] },
+				{ "responsivePriority": 2, "targets": [0, 2] },
+				{ "responsivePriority": 3, "targets": 4 }
+			]
 		});
 	}
 
