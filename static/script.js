1c0a1b981511c1da32700e7aa75f334f68e4312b
Tricholome <Tricholome@users.noreply.github.com>
2026-08-22 07:28:59 +0000

    deploy: Tricholome/rootelo@7ead62ff1bcde87d4848bc1186866875fbeef246

--- static/script.js
+++ static/script.js
@@ -691,7 +691,7 @@ $(document).ready(function() {
         if (dbBtn) {
             if (slug) {
                 const baseUrl = (window.PROFILE_BASE_URL || '').replace(/\/+$/, '');
-                dbBtn.href = `${baseUrl}/${slug}`;
+                dbBtn.href = `${baseUrl}/${slug}/#stats`;
                 dbBtn.classList.remove('disabled');
                 dbBtn.style.opacity = '1';
                 dbBtn.style.pointerEvents = 'auto';
